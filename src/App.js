18b017619a9e0f41bc4dec26f9a2c31fd7704e3c
Keen <keenballon@gmail.com>
2021-09-30 19:27:41 +0800

    the shop and collection prev page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
-
+import ShopPage from "./pages/shop/shop.component";
 class App extends Component {
     render() {
         return (
             <div>
-                <HomePage />
+                <Switch>
+                    <Route exact path="/" component={HomePage} />
+                    <Route exact path="/shop" component={ShopPage} />
+                </Switch>
             </div>
         );
     }
