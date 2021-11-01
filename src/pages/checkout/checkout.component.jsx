fa6f4bb3152ebe9d47f1199120263cb91592cc52
Keen <keenballon@gmail.com>
2021-11-01 23:37:47 +0800

    added stripe

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 import {
     selectCartItems,
     selectCartTotal,
@@ -31,6 +32,10 @@ const CheckoutPage = ({ cartItems, total }) => (
             <CheckoutItem key={cartItem.id} cartItem={cartItem} />
         ))}
         <div className="total">TOTAL: ${total}</div>
+        <div className="test-warning">
+            *Please use the following test credit card for payments* <br />
+        </div>
+        <StripeCheckoutButton price={total} />
     </div>
 );
 
